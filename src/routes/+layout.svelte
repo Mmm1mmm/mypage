4cdb2d4df3ade833f70a1ae1951c405d7c37eec2
Piano Nekomiya <kotone.olin1010@gmail.com>
2024-08-19 10:00:28 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import "../app.css";
+	import "$lib/cantarell.css";
+	import "@fontsource-variable/noto-sans-jp";
+	import Icon from "@iconify/svelte";
+</script>
+
+<nav class="fixed flex flex-col gap-2 p-2 h-full w-fit border-r bg-white dark:bg-zinc-900 border-zinc-200 dark:border-zinc-700 text-4xl">
+	<a href="/" title="ホーム"><Icon icon="pixelarticons:home" /></a>
+	<a href="/notification" title="通知"><Icon icon="pixelarticons:notification" /></a>
+	<a href="/friends" title="フレンド"><Icon icon="pixelarticons:users" /></a>
+	<a href="/send" title="送信"><Icon icon="pixelarticons:plus" /></a>
+</nav>
+
+<main>
+	<slot />
+</main>
